53d7cf014312a11ccbc960d088f9d1e44c45cfdb
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-04-27 23:42:38 +0100

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int shell(char *readline);
+char *get_the_line(void);
+char **parse_the_line(char *buff);
+int execute_the_line(char *buuf[]);
+void sigintHandler(__attribute__((unused)) int sig_num);
+#endif
