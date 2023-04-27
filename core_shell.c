e9bb616fde6db08f9a88c94e5b17629080a5cf09
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-04-27 23:34:06 +0100

    core shell

---
+++ core_shell.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * shell - the core of the shell
+ * @readline: the command ready for parsing and excution
+ * Return: the value returned from the excution
+ */
+
+int shell(char *readline)
+{
+	int f;
+	char **command = malloc(sizeof(char *) * 1024);
+
+	/**wait for user to enter commands */
+	command = parse_the_line(readline);
+	f = execute_the_line(command);
+	return (f);
+}
